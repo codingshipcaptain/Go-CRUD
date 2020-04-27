c5d0c0c160264bbc757cae0157bb346b575fa294
MathewUlanowski <mathew.ulanowski@gmail.com>
2020-04-26 22:06:24 -0500

    spawns the template folder and templates

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
     "database/sql"
     "log"
+    "os"
     "net/http"
     "text/template"
     _ "github.com/go-sql-driver/mysql"
@@ -17,6 +18,63 @@ type Employee struct {
     Id int
     Name string
     City string
+}
+
+// createFile makes a file it takes 3 parameters 
+// path (string) the child directory from the root that the file goes into DO NOT MAKE THIS A BLANK STRING
+// fileName (string) the name of the file and file type your writing
+// panic (bool) triggers panic if there is an error or continues to run and just logs the error
+func createFile(path string, fileName string, panicTrigger bool, content string) {
+    _, err := os.Stat(path+"/"+fileName)
+    if os.IsNotExist(err){
+        file, err := os.Create(path+"/"+fileName); defer file.Close()
+        if err != nil {
+            if panicTrigger {
+                panic(err.Error())
+            } else {
+                log.Println(err.Error())
+            }
+        } else {
+            file, err := os.OpenFile(path+"/"+fileName, os.O_RDWR, 0644); defer file.Close()
+            if err != nil {
+                if panicTrigger {
+                    panic(err.Error())
+                } else {
+                    log.Println(err.Error())
+                }
+            }
+            _, err = file.WriteString(content)
+            if err != nil{
+                if panicTrigger {
+                    panic(err.Error())
+                } else {
+                    log.Panicln(err.Error())
+                }
+            }
+            err = file.Sync()
+            if err != nil {
+                if panicTrigger {
+                    panic(err.Error())
+                } else {
+                    log.Panicln(err.Error())
+                }
+            }
+        }
+    }
+}
+
+// creating templates from thin air Note: programmed in a sleep deprived detrmination
+func createFileStructure() {
+    path := "form"
+    os.MkdirAll(path, 0777)
+    createFile(path, "Header.tmpl", false, "{{ define \"Header\" }} \n<!DOCTYPE html> \n<html lang=\"en-US\"> \n\t<head> \n\t\t<title>Golang MySQL CRUD Example</title> \n\t\t<meta charset=\"UTF-8\" /> \n\t</head> \n\t<body> \n\t\t<h1>Golang MySQL CRUD Example</h1> \n{{ end }}")
+    createFile(path, "Edit.tmpl", false, "{{ define \"Edit\" }}\n\t{{ template \"Header\" }}\n\t\t{{ template \"Menu\" }}\n\t\t<h2>Edit Name and City</h2>\n\t\t<form method=\"POST\" action=\"update\">\n\t\t\t<input type=\"hidden\" name=\"uid\" value=\"{{ .Id }}\" />\n\t\t\t<label> Name </label><input type=\"text\" name=\"name\" value=\"{{ .Name }}\" /><br />\n\t\t\t<label> City </label><input type=\"text\" name=\"city\" value=\"{{ .City }}\" /><br />\n\t\t\t<input type=\"submit\" value=\"Save user\" />\n\t\t</form><br />\n\t{{ template \"Footer\" }}\n{{ end }}")
+    createFile(path, "Footer.tmpl", false, "{{ define \"Footer\" }}\n</body>\n\n</html>\n{{ end }}")
+    createFile(path, "Index.tmpl", false, "{{ define \"Index\" }}\n\t{{ template \"Header\" }}\n\t\t{{ template \"Menu\"  }}\n\t\t<h2> Registered </h2>\n\t\t<table border=\"1\">\n\t\t\t<thead>\n\t\t\t\t<tr>\n\t\t\t\t\t<td>ID</td>\n\t\t\t\t\t<td>Name</td>\n\t\t\t\t\t<td>City</td>\n\t\t\t\t\t<td>View</td>\n\t\t\t\t\t<td>Edit</td>\n\t\t\t\t\t<td>Delete</td>\n\t\t\t\t</tr>\n\t\t\t</thead>\n\t\t\t<tbody>\n\t\t\t\t{{ range . }}\n\t\t\t\t<tr>\n\t\t\t\t\t<td>{{ .Id }}</td>\n\t\t\t\t\t<td> {{ .Name }} </td>\n\t\t\t\t\t<td>{{ .City }} </td>\n\t\t\t\t\t<td><a href=\"/show?id={{ .Id }}\">View</a></td>\n\t\t\t\t\t<td><a href=\"/edit?id={{ .Id }}\">Edit</a></td>\n\t\t\t\t\t<td><a href=\"/delete?id={{ .Id }}\">Delete</a></td>\n\t\t\t\t</tr>\n\t\t\t\t{{ end }}\n\t\t\t</tbody>\n\t\t</table>\n\t{{ template \"Footer\" }}\n{{ end }}")
+    createFile(path, "Menu.tmpl", false, "{{ define \"Menu\" }}\n<a href=\"/\">HOME</a> | \n<a href=\"/new\">NEW</a>\n{{ end }}")
+    createFile(path, "New.tmpl", false, "call New.tmpl file inside form.\n\n{{ define \"New\" }}\n\t{{ template \"Header\" }}\n\t\t{{ template \"Menu\" }}\n\t\t<h2>New Name and City</h2>\n\t\t<form method=\"POST\" action=\"insert\">\n\t\t\t<label> Name </label><input type=\"text\" name=\"name\" /><br />\n\t\t\t<label> City </label><input type=\"text\" name=\"city\" /><br />\n\t\t\t<input type=\"submit\" value=\"Save user\" />\n\t\t</form>\n\t{{ template \"Footer\" }}\n{{ end }}")
+    createFile(path, "Show.tmpl", false, "{{ define \"Show\" }}\n\t{{ template \"Header\" }}\n\t\t{{ template \"Menu\"  }}\n\t\t<h2> Register {{ .Id }} </h2>\n\t\t\t<p>Name: {{ .Name }}</p>\n\t\t\t<p>City:  {{ .City }}</p><br /> <a href=\"/edit?id={{ .Id }}\">Edit</a></p>\n\t{{ template \"Footer\" }}\n{{ end }}")
+
 }
 
 // dbInit Initializes a SQL DB for the web page
@@ -198,7 +256,8 @@ func main() {
     // this is run once to Initialize the DB upon running 
     // it will log some errors stating the db already exists if youve ran the program before dont worry it keeps going if that happens
     dbInit()
-    
+    createFileStructure()
+
     // URL routing
     log.Println("Server started on: http://localhost:8000")
 
